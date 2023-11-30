963ab0c69a0488fe83dd40a2780b430b54909a93
Ed <eduard.vorontsov@tages.ru>
2023-11-30 15:51:04 +0500

    sec init

--- protos/go.mod
+++ protos/go.mod
@@ -1,3 +1,8 @@
 module proto
 
 go 1.21.4
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	//TODO: config
+	//TODO: log
+	//TODO: app
+	//TODO: gRPC
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+	log.Debug("debug message")
+
+	log.Error("error message")
+
+	log.Warn("warn message")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+
+}
